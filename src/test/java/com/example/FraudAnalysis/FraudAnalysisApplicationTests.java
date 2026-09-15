6f2daacdece64d1db69c89c2f2fbfa93d14145c7
cauacouto <cauacouto022@gmail.com>
2026-09-15 20:22:09 -0300

    initialize FraudAnalysis project with Spring Boot and Maven setup

---
+++ src/test/java/com/example/FraudAnalysis/FraudAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FraudAnalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
