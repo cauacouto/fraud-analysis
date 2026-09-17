d08944b349102cfb344010524910dcc16a5d6f2e
cauacouto <cauacouto022@gmail.com>
2026-09-16 22:49:05 -0300

    add Kafka configuration for consumer with Avro deserialization

---
+++ src/main/java/com/example/FraudAnalysis/config/KafkaConfig.java
@@ -0,0 +1,53 @@
+package com.example.FraudAnalysis.config;
+
+import com.account.service.TransferEvent;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String BootsTrapServers;
+    @Value("${spring.kafka.properties.schema.registry.url}")
+     private String shemaRegistry;
+
+   @Bean
+    public ConsumerFactory<String, TransferEvent> consumerFactory(){
+        Map<String,Object> props = new HashMap<>();
+
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,BootsTrapServers);
+
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,"fraud-service-gorup");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,true);
+
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+
+        props.put("schema.registry.url",shemaRegistry);
+        props.put("specific.avro.reader",true);
+
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String,TransferEvent> kafkaListenerContainerFactory(){
+       ConcurrentKafkaListenerContainerFactory<String,TransferEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+       factory.setConsumerFactory(consumerFactory());
+       return factory;
+    }
+}
