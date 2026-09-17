a29cc78130a5a32733e552cf6716cade1d7b2364
cauacouto <cauacouto022@gmail.com>
2026-09-16 22:48:59 -0300

    add Lombok annotations and unique index to FraudAnalytic entity

--- src/main/java/com/example/FraudAnalysis/domin/FraudAnalytic.java
+++ src/main/java/com/example/FraudAnalysis/domin/FraudAnalytic.java
@@ -1,16 +1,22 @@
 package com.example.FraudAnalysis.domin;
 
 import com.example.FraudAnalysis.Enums.StatusTransfer;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 
 @Document(collation = "Analysis")
+@Getter
+@Setter
 public class FraudAnalytic {
 
-    @Id
+    @Id()
     private String id;
+    @Indexed(unique = true)
     private String idTransferecia;
     private String idOrigem;
     private String idDestino;
