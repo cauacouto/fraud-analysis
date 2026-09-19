ef0e70ec190b5b2ca25f494fa724b297aec2c0d2
cauacouto <cauacouto022@gmail.com>
2026-09-19 00:31:27 -0300

    add transfer validation logic to ConsumerService

--- src/main/java/com/example/FraudAnalysis/Service/ConsumerService.java
+++ src/main/java/com/example/FraudAnalysis/Service/ConsumerService.java
@@ -1,14 +1,22 @@
 package com.example.FraudAnalysis.Service;
 
 import com.account.service.TransferEvent;
+import com.example.FraudAnalysis.Enums.StatusMotivo;
+import com.example.FraudAnalysis.Enums.StatusTransfer;
+import com.example.FraudAnalysis.domin.FraudAnalytic;
+import com.example.FraudAnalysis.dto.ResultAnalitic;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Slf4j
 @Service
 public class ConsumerService {
 
+    private static final BigDecimal LIMITE_TRANSFERENCIA =
+            new BigDecimal("5000.00");
 
 @KafkaListener(
         topics = "transfer.created",
@@ -17,6 +25,33 @@ public class ConsumerService {
 )
 
     public void consumer(TransferEvent event){
-    log.info("transfericia recebida para analise ={}",event.getIdTransferencia());
+
+    ResultAnalitic analitic = verificaTransferencia (
+            event,
+            LIMITE_TRANSFERENCIA
+    );
+
+    log.info("Transferência {} recebida para análise",
+            event.getIdTransferencia()
+    );
 }
+
+
+
+
+
+ private ResultAnalitic verificaTransferencia(TransferEvent transferEvent, BigDecimal limiteTraferecia){
+
+   if (transferEvent.getValor().compareTo(limiteTraferecia) > 0){
+       return  new ResultAnalitic(
+               StatusTransfer.REJEITADO,
+               StatusMotivo.VALOR_ACIMA_DO_LIMITE
+       );
+   }
+   return new ResultAnalitic(
+           StatusTransfer.APROVADO,
+           null
+   );
+
+ }
 }
